6a294d328da50876ecd8407e7b6c4ce99af149bb
maria1412 <82562294+maria1412@users.noreply.github.com>
2021-12-22 14:19:35 +0900

    更新 main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,31 @@
 tiles.setTilemap(tilemap`レベル1`)
 let Flag = 0
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . b b b b b b . . . . . 
+    . . . b b 9 9 9 9 9 9 b b . . . 
+    . . b b 9 9 9 9 9 9 9 9 b b . . 
+    . . b b 9 d 9 9 9 9 9 9 b b . . 
+    . b b 9 d 9 9 9 9 1 1 9 9 b b . 
+    . b 9 d 9 9 9 9 9 1 1 9 9 9 b . 
+    . b 9 3 9 9 9 9 9 9 9 1 9 9 b . 
+    . b 5 3 d 9 9 9 9 9 9 9 9 9 b . 
+    . b 5 3 3 9 9 9 9 9 9 9 d 9 b . 
+    . b b 5 3 3 3 9 9 9 d d 5 b b . 
+    . . b d 5 3 3 3 3 3 d 5 b b . . 
+    . . . b d 5 d 3 3 3 5 5 b b . . 
+    . . . b b 5 5 5 5 5 5 b b . . . 
+    . . . . . b b b b b b . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+mySprite.setPosition(0, 23)
+controller.moveSprite(mySprite)
 game.onUpdate(function () {
     if (Flag == 1) {
         tiles.setTilemap(tilemap`レベル2`)
     } else if (Flag == 2) {
-        tiles.setTilemap(tilemap`レベル3`)
-    } else if (Flag == 3) {
         tiles.setTilemap(tilemap`レベル4`)
-    } else if (Flag == 4) {
+    } else if (Flag == 3) {
         tiles.setTilemap(tilemap`レベル5`)
     }
 })
