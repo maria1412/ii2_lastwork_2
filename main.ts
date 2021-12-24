adb3c45f96f34d825b43d15ff04ddfa21e0f9a22
maria1412 <82562294+maria1412@users.noreply.github.com>
2021-12-24 17:46:19 +0900

    更新 main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,25 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
+    tiles.setTilemap(tilemap`レベル4`)
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(14, 0))
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile13`, function (sprite, location) {
+    tiles.setTilemap(tilemap`レベル5`)
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 0))
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
+    tiles.setTilemap(tilemap`レベル2`)
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 1))
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    controller.moveSprite(mySprite, 300, 300)
+})
+controller.B.onEvent(ControllerButtonEvent.Released, function () {
+    controller.moveSprite(mySprite, 100, 100)
+})
+let mySprite: Sprite = null
 tiles.setTilemap(tilemap`レベル1`)
 let Flag = 0
-let mySprite = sprites.create(img`
+mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . b b b b b b . . . . . 
     . . . b b 9 9 9 9 9 9 b b . . . 
@@ -18,14 +37,6 @@ let mySprite = sprites.create(img`
     . . . . . b b b b b b . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-mySprite.setPosition(0, 23)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 1))
+scene.cameraFollowSprite(mySprite)
 controller.moveSprite(mySprite)
-game.onUpdate(function () {
-    if (Flag == 1) {
-        tiles.setTilemap(tilemap`レベル2`)
-    } else if (Flag == 2) {
-        tiles.setTilemap(tilemap`レベル4`)
-    } else if (Flag == 3) {
-        tiles.setTilemap(tilemap`レベル5`)
-    }
-})
